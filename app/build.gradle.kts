480a90b64ca6b5c646ccfb630b6fb2d7be162701
Binondi <binondiborthakur56@gmail.com>
2025-06-04 12:20:51 +0530

    Added - relatable dialog text.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "devs.org.calculator"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
