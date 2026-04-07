4cd6b4f502df29ff6244b831c90bf52c9538f04d
Binondi Borthakur <141143315+Binondi@users.noreply.github.com>
2026-04-07 13:42:42 +0530

    Added calculation history, fixed Go to home bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,9 +13,8 @@ android {
         minSdk = 26
         //noinspection OldTargetApi
         targetSdk = 34
-        versionCode = 12
-        versionName = "2.0.1"
-
+        versionCode = 13
+        versionName = "2.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
